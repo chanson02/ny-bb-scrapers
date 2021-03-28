788ffdbdc8325649a01c7f773374ca37dc43f247
Cooper <cooperhanson2020@gmail.com>
2021-03-28 14:59:05 -0500

    Price Rite fixed

--- Scrapers/price_rite_scraper.py
+++ Scrapers/price_rite_scraper.py
@@ -21,22 +21,19 @@ driver.get("https://www.priceritemarketplace.com/store")
 chain = {"name": "Price Rite", "stores": []}
 default_delay = 0.5
 
-locations = driver.find_elements_by_class_name("StoreItem-sc-kocgfr")
+locations = [l for l in driver.find_elements_by_class_name(
+    "StoreAddress-sc-wesvpf") if l.text != ""]
 for location in locations:
-    if location.text == "":
-        # Empty location
-        continue
+    ActionChains(driver).move_to_element(location).click().perform()
+    time.sleep(default_delay)
 
-    data = location.find_element_by_class_name(
-        "StoreAddress-sc-wesvpf").text.split("\n")
-    address = ", ".join(data[:2])
-
-    try:
-        remote_id = data[2]
+    data = location.text.split("\n")
+    remote_id = data[-1]
+    if remote_id.isdigit():
         phone = f"{remote_id[:3]}-{remote_id[3:6]}-{remote_id[6:]}"
-    except IndexError:
-        remote_id = None
-        phone = None
+    else:
+        phone = remote_id
+    address = ", ".join(data[:-1])
 
     store = {"address": address, "phone": phone, "id": remote_id}
     chain["stores"].append(store)
