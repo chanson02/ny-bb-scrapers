e01b581b22718239db6669a435591b375c14a00b
Cooper <cooperhanson2020@gmail.com>
2021-03-29 00:00:25 -0500

    Save A Lot added

--- Scrapers/save_alot_scraper.py
+++ Scrapers/save_alot_scraper.py
@@ -14,52 +14,46 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 from selenium.common import exceptions
 
-default_delay = 0.5
+default_delay = 2
 
 
-def get_hrefs():
-    content = driver.find_element_by_class_name("content")
-    hrefs = [anchor.get_attribute(
-        "href") for anchor in content.find_elements_by_tag_name("a")[1:]]
-    return hrefs
-
-
-chain = {"name": "Save a Lot", "stores": []}
+# chain = {"name": "Save A Lot", "stores": []}
+with open("../Outputs/save_alot.json", "r") as f:
+    chain = json.load(f)
 driver = webdriver.Chrome(executable_path='../chromedriver.exe')
-driver.get("https://savealot.com/grocery-stores/")
+driver.get("https://savealot.com/grocery-stores-near-me")
 time.sleep(default_delay)
 
-city_urls = []
-location_urls = []
-for state_url in get_hrefs():
-    driver.get(state_url)
+with open("../states.json") as f:
+    states = json.load(f)[30:]
+
+iframe = driver.find_element_by_id("sb-site-embed-iframe")
+driver.switch_to.frame(iframe)
+
+search_bar = driver.find_element_by_id("id_location")
+for state in states:
+    search_bar.send_keys(Keys.BACKSPACE * 50)
+    search_bar.send_keys(state["name"])
+    search_bar.send_keys(Keys.RETURN)
     time.sleep(default_delay)
-    city_urls += get_hrefs()
 
-for city_url in city_urls:
-    try:
-        driver.get(city_url)
-        time.sleep(default_delay)
-        location_urls += get_hrefs()[1:]
-    except:
-        pdb.set_trace()
+    locations = driver.find_elements_by_class_name("sb-location")
+    for location in locations:
+        ActionChains(driver).move_to_element(location).click().perform()
+        address = location.find_elements_by_tag_name(
+            "p")[1].text.replace("\n", ", ")
+        phone = location.find_element_by_class_name(
+            "phone-directions").find_element_by_tag_name("p").text[1:].replace(") ", "-")
 
-for location_url in location_urls:
-    try:
-        driver.get(location_url)
-        time.sleep(default_delay)
-
-        address = driver.find_element_by_class_name(
-            "address").text.replace("\n", ", ")
-        phone = driver.find_element_by_class_name(
-            "phone").text[1:].replace(") ", "-")
-        remote_id = location_url.split("-")[-1][:-1]
+        url = location.find_element_by_class_name(
+            "sb-location-link-view-location").find_element_by_tag_name("a").get_attribute("href")
+        remote_id = url.split("-")[-1]
 
         store = {"address": address, "phone": phone, "id": remote_id}
-        chain["stores"].append(store)
-        print("Added", store)
-    except:
-        pdb.set_trace()
+        if store not in chain["stores"]:
+            chain["stores"].append(store)
+            print("Added", store)
+
 
 with open("../Outputs/save_alot.json", "w") as f:
     json.dump(chain, f, indent=2)
